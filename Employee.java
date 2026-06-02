500e42e033555622db5ce6cc032d540220a682af
Rasikasutavane <rsutavane0404@gmail.com>
2026-06-02 16:26:25 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,16 @@
+package classandobjects;
+
+public class Employee {
+ int empid;
+ String name;
+ double sal;
+ String city;
+  void m4() {
+	  System.out.println("--- Employee Information---");
+	  System.out.println("Employee ID-->"+empid);
+	  System.out.println("Employee Name-->"+name);
+	  System.out.println("Employee Salary "+sal);
+	  System.out.println("Employee City"+city);
+	  
+  }
+}
---
+++ EmployeeMain.java
@@ -0,0 +1,13 @@
+package classandobjects;
+
+public class EmployeeMain {
+public static void main(String[] args) {
+	Employee emp=new Employee();
+	emp.name=" Sudesh Sharma";
+	emp.city=" Pune";
+	emp.empid=8765;
+	emp.sal=700000;
+	emp.m4();
+	
+}
+}
