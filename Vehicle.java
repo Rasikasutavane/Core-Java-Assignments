7e7244f385741c2f151544bd08d739d0df5fcf7b
Rasikasutavane <rsutavane0404@gmail.com>
2026-06-02 16:24:02 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,21 @@
+package classandobjects;
+
+public class Vehicle {
+  String name;
+  String carNo;
+  String brand;
+  long price;
+  int year;
+   void m1() {
+	   System.out.println("----Car Information----");
+	     System.out.println(" Car Name -->"+name);
+	     System.out.println(" Car Brand -->"+brand);
+	     System.out.println(" Model Year-->"+year);
+	     System.out.println(" Price of car-->"+price);
+	     System.out.println(" Car Number-->"+carNo);
+
+
+   }
+   
+  
+}
---
+++ VehicleMain.java
@@ -0,0 +1,13 @@
+package classandobjects;
+
+public class VehicleMain {
+public static void main(String[] args) {
+	Vehicle car =new Vehicle();
+	car.name="BMW X5";
+	car.brand="BMW";
+	car.price=9540000L;
+	car.year=2025;
+	car.carNo="MH6567";
+	car.m1();
+}
+}
