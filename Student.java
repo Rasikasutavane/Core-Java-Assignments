22411e0f2391d996fb4ac2477df8a2871657ac76
Rasikasutavane <rsutavane0404@gmail.com>
2026-06-02 14:50:21 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,15 @@
+package classandobjects;
+
+public class Student {
+
+		// TODO Auto-generated method stub
+     String name;
+     long mobN0;
+     String city;
+     int marks;
+     int attendance;
+     
+	}
+
+
+
