7d25bbaad264099bab1e92fc0705ab5fd077afc6
Rasikasutavane <rsutavane0404@gmail.com>
2026-06-02 16:25:36 +0530

    Add files via upload

---
+++ Hospital.java
@@ -0,0 +1,20 @@
+package classandobjects;
+
+public class Hospital {
+	 String name;
+	  String doctorname;
+	  String city;
+	   int doctors;
+	   int totalRoom;
+	   void m2() {
+		   System.out.println("----Hosptal Information----");
+		     System.out.println(" Hosptal Name -->"+name);
+		     System.out.println(" City -->"+city);
+		     System.out.println(" Doctor name-->"+doctorname);
+		     System.out.println(" Total Rooms-->"+totalRoom);
+		     System.out.println(" Total Doctors-->"+doctors); 
+	   }
+	   
+	   
+	   
+}
---
+++ HospitalMain.java
@@ -0,0 +1,16 @@
+package classandobjects;
+
+public class HospitalMain {
+
+	public static void main(String[] args) {
+    Hospital hosp=new Hospital();   
+     hosp.name=" City Hospital";
+     hosp.city="Yavatmal";
+     hosp.doctorname=" Dr.Hathi";
+     hosp.totalRoom=250;
+     hosp.doctors=50;
+     hosp.m2();
+	}
+	
+}
+
