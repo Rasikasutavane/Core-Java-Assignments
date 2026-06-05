85eeeb1d9b3a4e20c9bc7faeb14940a7accf9df9
Rasikasutavane <rsutavane0404@gmail.com>
2026-06-05 14:09:27 +0530

    Add files via upload

---
+++ methods/CustomerMain.java
@@ -0,0 +1,15 @@
+package methods;
+
+public class CustomerMain {
+
+	public static void main(String[] args) {
+		Customer c1=new Customer();
+		c1.city="Pune";
+		c1.id=101;
+		c1.mobno=898989888;
+		c1.name="Rasika";
+		c1.orderid=5055;
+		c1.show();
+	}
+
+}
