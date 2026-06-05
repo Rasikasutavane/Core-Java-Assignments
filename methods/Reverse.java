813eb41e00b3827697e08dfbe7bc8100435eaac3
Rasikasutavane <rsutavane0404@gmail.com>
2026-06-05 13:37:00 +0530

    Add files via upload

---
+++ methods/Reverse.java
@@ -0,0 +1,15 @@
+package methods;
+
+public class Reverse {
+ public void re (long a) {long rev1=0;
+	 while(a!=0) {
+	 long digit=a%10;
+	  rev1 *=10+digit;
+	  a=a/10;}
+	 System.out.println(" Reverse"+rev1);}
+	 
+	 
+ }
+
+  
+
