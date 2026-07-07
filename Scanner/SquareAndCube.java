ca4f98259df668c047af3c145437669e4d1b1f97
Rasikasutavane <rsutavane0404@gmail.com>
2026-07-07 16:59:43 +0530

    Add files via upload

---
+++ Scanner/SquareAndCube.java
@@ -0,0 +1,15 @@
+package Scanner;
+import java.util.Scanner;
+public class SquareAndCube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc= new Scanner( System.in);
+ System.out.println(" Enter Number ");
+ float n=sc.nextFloat();
+ 
+System.out.println(" Square : "+(n*n)+" Cube :"+(n*n*n));
+sc.close();
+	}
+
+}
