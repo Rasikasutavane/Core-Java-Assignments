9942a66cd568717c30cdd058bab7087374f50d37
Rasikasutavane <rsutavane0404@gmail.com>
2026-07-09 18:24:35 +0530

    Add files via upload

---
+++ class-and-object/Mobile.java
@@ -0,0 +1,19 @@
+package classandobjects;
+
+public class Mobile {
+
+String brand;
+String ram;
+long price;
+String storage;
+
+void displayspec() {
+	System.err.println(" -----Mobile Inforamation-----");
+	System.out.println(" Brand name :"+brand);
+	System.out.println(" Ram :"+ram);
+	System.out.println(" Storage :"+storage);
+	System.out.println(" Price :"+price);
+	
+}
+
+}
---
+++ class-and-object/MobileMain.java
@@ -0,0 +1,12 @@
+package classandobjects;
+
+public class MobileMain {
+public static void main(String[] args) {
+	Mobile m=new Mobile();
+	m.brand="SAMSUNG";
+	m.price=450000;
+	m.ram=" 12GB";
+	m.storage="526GB";
+	m.displayspec();
+}
+}
