734d4a2928e96898ceb04142f4b5360f1b01e827
Rasikasutavane <rsutavane0404@gmail.com>
2026-07-24 16:15:30 +0530

    Delete Inheritance/Animal/inheritance directory

--- Inheritance/Animal/inheritance/A.java
+++
@@ -1,7 +0,0 @@
-package inheritance;
-
-public class A {
-	public void show() {
-		System.out.println(" This is method show() of class A");
-	}
-}
--- Inheritance/Animal/inheritance/X.java
+++
@@ -1,6 +0,0 @@
-package inheritance;
-
-public class X {
-
-	int x=10;
-}
